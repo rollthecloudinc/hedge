84112567c3c985934e28a803d5ad52d4e5ca16f8
Todd Zmijewski <sme.s.key@gmail.com>
2020-05-17 08:44:45 -0400

    Initial commit for go classifieds api.

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type response struct {
+	UTC time.Time `json:"utc"`
+}
+
+func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	now := time.Now()
+	resp := &response{
+		UTC: now.UTC(),
+	}
+	body, err := json.Marshal(resp)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+	return events.APIGatewayProxyResponse{Body: string(body), StatusCode: 200}, nil
+}
+func main() {
+	lambda.Start(handleRequest)
+}

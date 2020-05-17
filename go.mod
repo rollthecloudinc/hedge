9b56e0229c3c77b0fac7ddf3dc2e351c4f6e9379
Todd Zmijewski <sme.s.key@gmail.com>
2020-05-17 09:42:43 -0400

    Functional bare bone api using aws lambda and gin.

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.14
 
 require (
 	github.com/aws/aws-lambda-go v1.16.0
-	github.com/awslabs/aws-lambda-go-api-proxy v0.6.0 // indirect
-	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/awslabs/aws-lambda-go-api-proxy v0.6.0
+	github.com/gin-gonic/gin v1.6.3
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
--- main.go
+++ main.go
@@ -2,28 +2,34 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"time"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
 )
 
-type response struct {
-	UTC time.Time `json:"utc"`
+var ginLambda *ginadapter.GinLambda
+
+func init() {
+	// stdout and stderr are sent to AWS CloudWatch Logs
+	log.Printf("Gin cold start")
+	r := gin.Default()
+	r.GET("/ads/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	ginLambda = ginadapter.New(r)
 }
 
-func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	now := time.Now()
-	resp := &response{
-		UTC: now.UTC(),
-	}
-	body, err := json.Marshal(resp)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-	return events.APIGatewayProxyResponse{Body: string(body), StatusCode: 200}, nil
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// If no name is provided in the HTTP request body, throw an error
+	return ginLambda.ProxyWithContext(ctx, req)
 }
+
 func main() {
-	lambda.Start(handleRequest)
+	lambda.Start(Handler)
 }

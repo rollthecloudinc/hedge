9d736e968c556ed8c4f8dbe09cae7e0144b994ae
Todd Zmijewski <sme.s.key@gmail.com>
2020-06-15 08:37:48 -0400

    Create chat connections when connection established to stream.

--- api/stream/main.go
+++ api/stream/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"crypto/tls"
+	"encoding/json"
 	"fmt"
+	"goclassifieds/lib/entity"
 	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	session "github.com/aws/aws-sdk-go/aws/session"
+	lambda2 "github.com/aws/aws-sdk-go/service/lambda"
+	"github.com/gocql/gocql"
 )
 
 var handler Handler
@@ -13,21 +19,82 @@ var handler Handler
 type Handler func(req *events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error)
 
 type ActionContext struct {
-	UserId string
+	Session     *gocql.Session
+	Lambda      *lambda2.Lambda
+	UserId      string
+	ConnManager entity.EntityManager
+}
+
+func Connect(req *events.APIGatewayWebsocketProxyRequest, ac *ActionContext) (events.APIGatewayProxyResponse, error) {
+	log.Print("connect")
+	//log.Print("user id = " + ac.UserId)
+	obj := make(map[string]interface{})
+	obj["connId"] = req.RequestContext.ConnectionID
+	_, err := ac.ConnManager.Create(obj)
+	if err != nil {
+		log.Print(err)
+		return events.APIGatewayProxyResponse{StatusCode: 500}, err
+	}
+	return events.APIGatewayProxyResponse{StatusCode: 200}, nil
+}
+
+func Disconnect(req *events.APIGatewayWebsocketProxyRequest, ac *ActionContext) (events.APIGatewayProxyResponse, error) {
+	log.Print("disconnect")
+	return events.APIGatewayProxyResponse{StatusCode: 200}, nil
 }
 
 func InitializeHandler(c *ActionContext) Handler {
 	return func(req *events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
-		//ac := RequestActionContext(c)
-		// ac.UserId = GetUserId(req)
-		log.Printf("connection id = %s", req.RequestContext.ConnectionID)
-		log.Printf("user id = %s", GetUserId(req))
-		return events.APIGatewayProxyResponse{StatusCode: 200}, nil
+
+		ac := RequestActionContext(c)
+
+		ac.UserId = GetUserId(req)
+		ac.ConnManager = CreateConnectionManager(ac)
+
+		b, _ := json.Marshal(req)
+		log.Print(string(b))
+
+		if req.RequestContext.RouteKey == "$connect" {
+			return Connect(req, ac)
+		} else if req.RequestContext.RouteKey == "$disconnect" {
+			return Disconnect(req, ac)
+		}
+
+		return events.APIGatewayProxyResponse{StatusCode: 500}, nil
 	}
 }
 
-func RequestActionContext(ac *ActionContext) *ActionContext {
-	return &ActionContext{}
+func CreateConnectionManager(ac *ActionContext) entity.EntityManager {
+	manager := entity.EntityManager{
+		Config: entity.EntityConfig{
+			SingularName: "chatconnection",
+			PluralName:   "chatconnections",
+			IdKey:        "connId",
+		},
+		Creator: entity.DefaultCreatorAdaptor{
+			Config: entity.DefaultCreatorConfig{
+				Lambda: ac.Lambda,
+				UserId: ac.UserId,
+				Save:   "default",
+			},
+		},
+		Storages: map[string]entity.Storage{
+			"default": entity.CqlStorageAdaptor{
+				Config: entity.CqlAdaptorConfig{
+					Session: ac.Session,
+					Table:   "chatconnections",
+				},
+			},
+		},
+	}
+	return manager
+}
+
+func RequestActionContext(c *ActionContext) *ActionContext {
+	return &ActionContext{
+		Session: c.Session,
+		Lambda:  c.Lambda,
+	}
 }
 
 func GetUserId(req *events.APIGatewayWebsocketProxyRequest) string {
@@ -42,7 +109,26 @@ func GetUserId(req *events.APIGatewayWebsocketProxyRequest) string {
 }
 
 func init() {
-	actionContext := ActionContext{}
+	cluster := gocql.NewCluster("cassandra.us-east-1.amazonaws.com")
+	cluster.Keyspace = "ClassifiedsDev"
+	cluster.Port = 9142
+	cluster.Consistency = gocql.LocalQuorum
+	cluster.Authenticator = &gocql.PasswordAuthenticator{Username: "tzmijewski-at-989992233821", Password: "oALqeCqjS3BgyiBp2Ram8kTUbhttAYoyUoL70hmz+tY="}
+	cluster.SslOpts = &gocql.SslOptions{Config: &tls.Config{ServerName: "cassandra.us-east-1.amazonaws.com"}, CaPath: "api/chat/AmazonRootCA1.pem", EnableHostVerification: true}
+	cluster.PoolConfig = gocql.PoolConfig{HostSelectionPolicy: /*gocql.TokenAwareHostPolicy(*/ gocql.DCAwareRoundRobinPolicy("us-east-1") /*)*/}
+	cSession, err := cluster.CreateSession()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sess := session.Must(session.NewSession())
+	lClient := lambda2.New(sess)
+
+	actionContext := ActionContext{
+		Session: cSession,
+		Lambda:  lClient,
+	}
+
 	handler = InitializeHandler(&actionContext)
 }
 

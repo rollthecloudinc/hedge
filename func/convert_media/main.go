58714ca3adcb00258f7956630e128f70ea88ffea
angular druid <angular.druid@gmail.com>
2021-06-11 19:04:09 -0400

    Pass request with css content to node conversion program.

--- func/convert_media/main.go
+++ func/convert_media/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"log"
 	"os"
 
@@ -10,9 +12,15 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	lambda2 "github.com/aws/aws-sdk-go/service/lambda"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 var stage string
+
+type CssToJsonRequest struct {
+	Content string
+}
 
 func handler(ctx context.Context, s3Event events.S3Event) {
 
@@ -23,7 +31,10 @@ func handler(ctx context.Context, s3Event events.S3Event) {
 
 		log.Printf("%+v", record)
 
-		payload := []byte("")
+		content, _ := download(sess, &record)
+		payload, _ := request(content)
+
+		log.Printf("%s", payload)
 
 		res, err := lClient.Invoke(&lambda2.InvokeInput{FunctionName: aws.String("goclassifieds-api-" + stage + "-CssToJson"), Payload: payload})
 		if err != nil {
@@ -31,9 +42,41 @@ func handler(ctx context.Context, s3Event events.S3Event) {
 			log.Printf("response: %s", res)
 		}
 
-		// pieces := strings.Split(record.S3.Object.Key, "/")
-
 	}
+}
+
+func request(content []byte) ([]byte, error) {
+
+	request := CssToJsonRequest{
+		Content: string(content),
+	}
+
+	payload, err := json.Marshal(request)
+	if err != nil {
+		log.Printf("Error marshalling css to json request: %s", err.Error())
+		return nil, errors.New("Error marshalling css to json request")
+	}
+
+	return payload, nil
+}
+
+func download(sess *session.Session, record *events.S3EventRecord) ([]byte, error) {
+
+	buf := aws.NewWriteAtBuffer([]byte{})
+
+	downloader := s3manager.NewDownloader(sess)
+
+	_, err := downloader.Download(buf, &s3.GetObjectInput{
+		Bucket: aws.String(record.S3.Bucket.Name),
+		Key:    aws.String(record.S3.Object.Key),
+	})
+
+	if err != nil {
+		return []byte(""), err
+	}
+
+	return buf.Bytes(), nil
+
 }
 
 func main() {
--- func/css_to_json/index.js
+++ func/css_to_json/index.js
@@ -2,5 +2,7 @@ var cssparser = require("cssparser");
 
 module.exports.handler = function(event, context, callback) {
   console.log('hello world');
+  console.log(event);
+  console.log(context);
   callback();
 }

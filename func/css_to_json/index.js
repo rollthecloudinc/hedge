4bb10e2b633be0f67da3b1c838aa39b2cdc9018b
angular druid <angular.druid@gmail.com>
2021-06-11 20:35:51 -0400

    Replace cssparser because its garbage.

--- func/css_to_json/index.js
+++ func/css_to_json/index.js
@@ -1,8 +1,10 @@
-var cssparser = require("cssparser");
+var cssjson = require("cssjson");
 
 module.exports.handler = function(event, context, callback) {
   console.log('hello world');
   console.log(event);
   console.log(context);
-  callback();
+  var json = cssjson.toJSON(event.Content);
+  console.log(json);
+  callback(null, json);
 }

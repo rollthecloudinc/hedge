a0d0020a060b3386c22c5551f1c1087f1cc78454
druid <angular.druid@gmail.com>
2022-11-08 20:33:20 -0500

    [tz-85] Optimze OpenSearch storage capacity using smaller units and convert to renewable_record lambda.

--- func/renewable_record/main.go
+++ func/renewable_record/main.go
@@ -4,10 +4,10 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
 	"goclassifieds/lib/entity"
 	"goclassifieds/lib/sign"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -21,13 +21,13 @@ import (
 type RenewableRecord struct {
 	RequestId      string `json:"request_id"`
 	Region         string `json:"region"`
-	Duration       string `json:"duration"`
-	BilledDuration string `json:"billed_duration"`
-	MemorySize     string `json:"memory_size"`
-	MaxMemoryUsed  string `json:"max_memory_used"`
-	InitDuration   string `json:"init_duration"`
-	Intensity      string `json:"intensity"`
-	Electricity    string `json:"electricity"`
+	Duration       uint16 `json:"duration"`
+	BilledDuration uint16 `json:"billed_duration"`
+	MemorySize     uint16 `json:"memory_size"`
+	MaxMemoryUsed  uint16 `json:"max_memory_used"`
+	InitDuration   uint16 `json:"init_duration"`
+	Intensity      uint16 `json:"intensity"`
+	Electricity    uint16 `json:"electricity"`
 	Function       string `json:"function"`
 	Path           string `json:"path"`
 }
@@ -40,47 +40,60 @@ func handler(ctx context.Context, logsEvent events.CloudwatchLogsEvent) {
 	data, _ := logsEvent.AWSLogs.Parse()
 	records := make([]RenewableRecord, 0)
 	for _, logEvent := range data.LogEvents {
+		log.Print(logEvent.Message)
 		pieces := strings.Fields(logEvent.Message)
 		lastNumberIndex := -1
 		record := RenewableRecord{
 			Region: os.Getenv("AWS_REGION"),
 		}
 		for index, field := range pieces {
-			val, err := strconv.ParseFloat(field, 32)
-			_, err2 := strconv.Atoi(field)
+			_, err := strconv.ParseFloat(field, 32)
+			ival, err2 := strconv.ParseUint(field, 0, 16)
 			if err == nil && err2 == nil {
 				if lastNumberIndex == -1 && len(pieces[index-2]) > 30 {
 					lastNumberIndex = index - 3
 				}
 				name := strings.Join(pieces[lastNumberIndex+2:index], " ")
 				if name == "Duration:" {
-					record.Duration = fmt.Sprintf("%f%s", val, strings.ToLower(pieces[index+1]))
+					// record.Duration = fmt.Sprintf("%f%s", val, strings.ToLower(pieces[index+1]))
+					record.Duration = uint16(ival)
 				} else if name == "Billed Duration:" {
-					record.BilledDuration = fmt.Sprintf("%f%s", val, strings.ToLower(pieces[index+1]))
+					// record.BilledDuration = fmt.Sprintf("%f%s", val, strings.ToLower(pieces[index+1]))
+					record.BilledDuration = uint16(ival)
 				} else if name == "Memory Size:" {
-					record.MemorySize = fmt.Sprintf("%f%s", val, strings.ToLower(pieces[index+1]))
+					// record.MemorySize = fmt.Sprintf("%f%s", val, strings.ToLower(pieces[index+1]))
+					record.MemorySize = uint16(ival)
 				} else if name == "Max Memory Used:" {
-					record.MaxMemoryUsed = fmt.Sprintf("%f%s", val, strings.ToLower(pieces[index+1]))
+					// record.MaxMemoryUsed = fmt.Sprintf("%f%s", val, strings.ToLower(pieces[index+1]))
+					record.MaxMemoryUsed = uint16(ival)
 				} else if name == "Init Duration:" {
-					record.InitDuration = fmt.Sprintf("%f%s", val, strings.ToLower(pieces[index+1]))
+					// record.InitDuration = fmt.Sprintf("%f%s", val, strings.ToLower(pieces[index+1]))
+					record.InitDuration = uint16(ival)
 				}
 				lastNumberIndex = index
 			} else if field == "RequestId:" && len(pieces) >= index {
 				record.RequestId = pieces[index+1]
 			} else if field == "Duration:" && len(pieces) >= index {
 				if pieces[index-1] == "Billed" {
-					record.BilledDuration = strings.Join(pieces[index+1:index+3], "")
+					// record.BilledDuration = strings.Join(pieces[index+1:index+3], "")
+					f, _ := strconv.ParseFloat(pieces[index+1], 32)
+					record.BilledDuration = uint16(math.Round(f))
 				} else if pieces[index-1] == "Init" {
-					record.InitDuration = strings.Join(pieces[index+1:index+3], "")
+					// record.InitDuration = strings.Join(pieces[index+1:index+3], "")
+					f, _ := strconv.ParseFloat(pieces[index+1], 32)
+					record.InitDuration = uint16(math.Round(f))
 				} else {
-					record.Duration = strings.Join(pieces[index+1:index+3], "")
+					// record.Duration = strings.Join(pieces[index+1:index+3], "")
+					f, _ := strconv.ParseFloat(pieces[index+1], 32)
+					record.Duration = uint16(math.Round(f))
 				}
 			} else if field == "Function:" && len(pieces) >= index {
 				record.Function = pieces[index+1]
 			} else if field == "Path:" && len(pieces) >= index {
 				record.Path = pieces[index+1]
 			} else if field == "Path:" && len(pieces) >= index {
-				record.Intensity = pieces[index+1]
+				f, _ := strconv.ParseFloat(pieces[index+1], 32)
+				record.Intensity = uint16(math.Round(f))
 			}
 		}
 		b, err := json.Marshal(record)

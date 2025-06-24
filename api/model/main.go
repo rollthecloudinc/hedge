9b4160cc10906259502e196aa148ce9c8c35d1c0
Todd Zmijewski <angular.druid@gmail.com>
2025-06-24 04:55:20 -0400

    Beginning of splits buildout.

---
+++ api/model/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"context"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       "Hello World",
+	}, nil
+}
+
+func main() {
+	log.SetFlags(0)
+	lambda.Start(handler)
+}
